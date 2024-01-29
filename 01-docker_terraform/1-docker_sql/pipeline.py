bcaa8cb4531e66d5f6dd6bb0db5e9d01c4672641
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2024-01-29 13:48:10 +0530

    Docker, sql, terraform basics with gcp provider

---
+++ 01-docker_terraform/1-docker_sql/pipeline.py
@@ -0,0 +1,15 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+# sys.argv[0] is name of the file
+# e.g. 
+# winpty docker run -it test:pandas 2024-01-13
+# ['pipeline.py', '2024-01-13']
+# job finished successfully for day = 2024-01-13
+
+day = sys.argv[1]
+
+print(f'job finished successfully for day = {day}')
---
+++ 01-docker_terraform/2-terraform_gcp/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-demo-412617"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "terraform-demo-412617-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
