7c82ff69ac7b6ea72cc2298618496720fc8c51e6
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2024-02-17 14:52:09 +0000

    Week-1 revisit

--- 1-docker-and-terraform/1-docker_sql/1-docker-basics/pipeline.py
+++ 1-docker-and-terraform/1-docker_sql/1-docker-basics/pipeline.py
@@ -6,7 +6,8 @@ print(sys.argv)
 
 # sys.argv[0] is name of the file
 # e.g. 
-# winpty docker run -it test:pandas 2024-01-13
+# winpty docker run -it test:pandas 2024-01-13 
+# (winpty on windows)
 # ['pipeline.py', '2024-01-13']
 # job finished successfully for day = 2024-01-13
 
